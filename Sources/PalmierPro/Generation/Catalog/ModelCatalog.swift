f86c0b9c92f7c920b3c533bdcc4bc694e8174fb5
Harrison Tin <harrison@palmier.io>
2026-07-27 18:08:36 -0700

    Normalize source videos for generation

--- Sources/PalmierPro/Generation/Catalog/ModelCatalog.swift
+++ Sources/PalmierPro/Generation/Catalog/ModelCatalog.swift
@@ -249,12 +249,17 @@ struct VideoCaps: Decodable, Sendable {
     let requiresSourceVideo: Bool
     let maxSourceVideoSeconds: Double?
     let maxSourceVideoResolution: SourceVideoResolution?
+    let requiredSourceVideoEncoding: SourceVideoEncoding?
     let requiresReferenceImage: Bool
     let requiresReferenceAudio: Bool?
 }
 
 enum SourceVideoResolution: String, Decodable, Sendable {
     case p720 = "720p", p1080 = "1080p", p4k = "4k"
+}
+
+enum SourceVideoEncoding: String, Decodable, Sendable {
+    case h264MP4 = "h264-mp4"
 }
 
 struct ImageCaps: Decodable, Sendable {
